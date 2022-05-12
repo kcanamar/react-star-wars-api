d76ff2962dd707808cff9e8f07c1fcbbb8081b0f
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-11 18:48:50 -0700

    creates working component, sucessful react app launch.

---
+++ src/components/Starships.js
@@ -0,0 +1,10 @@
+// import { useState, useEffect } from "react"
+
+export default function Starships(props) {
+    
+    return (
+        <div>
+            <h1>Star Wars</h1>
+        </div>
+    )
+}
