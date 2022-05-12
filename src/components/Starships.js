3c72e5286669d93b67f078404133286a2a03cc7d
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-11 20:43:04 -0700

    renders cards to the dom with correct api data

--- src/components/Starships.js
+++ src/components/Starships.js
@@ -1,10 +1,46 @@
-// import { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 
-export default function Starships(props) {
-    
+
+export default function Starships(_props) {
+
+    const [starships, setStarships] = useState([])
+    const getStarships = async () => {
+        const response = await fetch("https://swapi.dev/api/starships/")
+        const json = await response.json()
+        setStarships(json.results)
+    }
+
+    useEffect(() => { getStarships() }, [])
+    if (starships.length <= 0) {
+        return <div>
+            <h1>No Ships</h1>
+            <button onClick={getStarships}>Get Ships</button>
+        </div>
+    }
+
     return (
         <div>
             <h1>Star Wars</h1>
+
+            <div>
+                {starships.map((ship) => {
+                    return (
+                        <div key={ship.name}>
+                            <div className="row">
+                                <div className="col s12 m3">
+                                <div className="card blue-grey darken-1">
+                                    <div className="card-content white-text">
+                                    <span className="card-title">{ship.name}</span>
+                                    <p>Cost: {ship.cost_in_credits}</p>
+                                    <p>Made By --> {ship.manufacturer}</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                      </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
